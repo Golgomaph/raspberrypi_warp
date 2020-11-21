fccdfafb63936dec29eecad7d55a986b4cce9ffd
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-11-21 14:45:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,9 @@
 read -p "Hostname [$(hostname)]: " HOSTNAME
 sudo raspi-config nonint do_hostname ${HOSTNAME:-$(hostname)}
 
+echo "Updating packages"
+sudo apt update
+sudo apt upgrade -y
+
 echo "Installing components"
 sudo ./install-bluetooth.sh
