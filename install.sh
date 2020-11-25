d83f3f95788c421e4ad040abff5277a5cb425aa2
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-11-25 12:21:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/bin/bash -e
 
+read -p "Hostname [$(hostname)]: " HOSTNAME
+sudo raspi-config nonint do_hostname ${HOSTNAME:-$(hostname)}
+
+CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
+read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
+sudo hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
+
 echo "Updating packages"
 sudo apt update
-sudo apt upgrade -y
+#sudo apt upgrade -y
 
 echo "Installing components"
 sudo ./install-bluetooth.sh
@@ -13,12 +20,3 @@ sudo ./install-snapcast-client.sh
 sudo ./install-pivumeter.sh
 sudo ./enable-hifiberry.sh
 sudo ./enable-read-only.sh
-
-read -p "Hostname [$(hostname)]: " HOSTNAME
-sudo raspi-config nonint do_hostname ${HOSTNAME:-$(hostname)}
-
-CURRENT_PRETTY_HOSTNAME=$(hostnamectl status --pretty)
-read -p "Pretty hostname [${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}]: " PRETTY_HOSTNAME
-sudo hostnamectl set-hostname --pretty "${PRETTY_HOSTNAME:-${CURRENT_PRETTY_HOSTNAME:-Raspberry Pi}}"
-
-echo "Installation finished. Reboot now.."
