d472f732bbb5f10283d54ac41df2a558d234fb85
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-12-14 13:04:11 +0100

    Update install-spotify.sh

--- install-spotify.sh
+++ install-spotify.sh
@@ -2,7 +2,6 @@
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
-echo
 echo -n "Installing raspotify"
 #echo
 #echo -n "Do you want to see RasPi as streaming device in Spotify (Raspotify)? [y/N] "
