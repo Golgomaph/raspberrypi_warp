ba4e533e50a9b4b7d43c3a564bb18a2970cd32bd
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-12-14 12:09:33 +0100

    Update rebootafterfinish.sh

--- rebootafterfinish.sh
+++ rebootafterfinish.sh
@@ -1,4 +1,9 @@
-echo "Installation finished. Reboot now? [y/N]
+#!/bin/bash -e
+
+echo
+echo -n "----------------------------------------------------------------"
+echo
+echo -n "Installation finished. Reboot now? [y/N]"
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 sudo reboot
