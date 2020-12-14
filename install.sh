415c888041c71482e56e03dca10a5bbf2eafc38d
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-12-14 11:57:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ sudo apt upgrade -y
 
 echo "Installing components"
 sudo ./install-bluetooth.sh
+sudo ./install-upnp.sh
 sudo ./install-shairport.sh
 sudo ./install-spotify.sh
-sudo ./install-upnp.sh
 sudo ./install-snapcast-client.sh
 #sudo ./install-pivumeter.sh
-sudo ./enable-hifiberry.sh
-sudo ./enable-read-only.sh
+#sudo ./enable-hifiberry.sh
+#sudo ./enable-read-only.sh
