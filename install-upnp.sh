69a066bd9cc6d811d7f383c0ca79b1b4931104ab
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-12-14 12:01:54 +0100

    Update install-upnp.sh

--- install-upnp.sh
+++ install-upnp.sh
@@ -4,6 +4,7 @@ if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo
 echo -n "----------------------------------------------------------------"
+echo
 echo -n "Do you want to stream audio via WiFi-UPnP (gmrender-resurrect)? [y/N] "
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
