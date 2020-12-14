331b7e61250c476d9d66b37b282884cfd461c53a
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-12-14 12:56:10 +0100

    Update install-pivumeter.sh

--- install-pivumeter.sh
+++ install-pivumeter.sh
@@ -3,9 +3,9 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo
-echo -n "Do you want to install ALSA VU meter plugin (pivumeter) [y/N] "
-read REPLY
-if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
+echo -n "Installing pivumeter"
+#read REPLY
+#if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
 apt install --no-install-recommends -y git build-essential autoconf automake libtool libasound2-dev libfftw3-dev wiringpi
 git clone https://github.com/pimoroni/pivumeter.git
