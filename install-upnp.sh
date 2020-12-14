2cae2410508c68c3527976b984792b4240e4d177
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-12-14 12:59:09 +0100

    Update install-upnp.sh

--- install-upnp.sh
+++ install-upnp.sh
@@ -3,11 +3,11 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo
-echo -n "----------------------------------------------------------------"
-echo
-echo -n "Do you want to stream audio via WiFi-UPnP (gmrender-resurrect)? [y/N] "
-read REPLY
-if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
+echo -n "Installing gmrender-resurrect"
+#echo
+#echo -n "Do you want to stream audio via WiFi-UPnP (gmrender-resurrect)? [y/N] "
+#read REPLY
+#if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
 apt install -y --no-install-recommends gmediarender gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-alsa
 
