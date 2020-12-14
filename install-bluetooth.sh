4d4091aa144d24dbc8e0b94fad0895174e334d63
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-12-14 11:48:35 +0100

    Update install-bluetooth.sh

--- install-bluetooth.sh
+++ install-bluetooth.sh
@@ -3,7 +3,7 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo
-echo -n "Do you want to install Bluetooth Audio (BlueALSA)? [y/N] "
+echo -n "Do you want to stream audio via bluetooth? [y/N] "
 read REPLY
 if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
