d47c9cc90c6d578c05fb3d575abcbcfb3b07121d
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-12-14 12:58:14 +0100

    Update install-snapcast-client.sh

--- install-snapcast-client.sh
+++ install-snapcast-client.sh
@@ -3,10 +3,10 @@
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 
 echo
-echo -n "----------------------------------------------------------------"
-echo
-echo -n "Do you want to use multi-room functionality using more then one RasPi (snapclient})? [y/N] "
-read REPLY
-if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
+echo -n "Installing snapclient"
+#echo
+#echo -n "Do you want to use multi-room functionality using more then one RasPi (snapclient})? [y/N] "
+#read REPLY
+#if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
 
 apt install --no-install-recommends -y snapclient
