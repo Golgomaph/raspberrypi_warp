5894f500445d98ef50f9738bd12ce576f152fa68
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2020-12-15 17:46:43 +0100

    Update install_autoap.sh

--- install_autoap.sh
+++ install_autoap.sh
@@ -1,1 +1,20 @@
+#!/bin/bash -e
 
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+
+echo "Installing Auto Access Point"
+
+sudo apt-get -f install npm
+sudo npm install bower -g
+#sudo apt-get install isc-dhcp-server
+sudo apt-get install git
+git clone https://github.com/sabhiram/raspberry-wifi-conf.git
+cd raspberry-wifi-conf
+npm update
+bower install
+sudo npm run-script provision
+sudo npm start
+sudo cp assets/init.d/raspberry-wifi-conf /etc/init.d/raspberry-wifi-conf
+sudo chmod +x /etc/init.d/raspberry-wifi-conf
+sudo systemctl unmask hostapd
+sudo systemctl enable hostapd
