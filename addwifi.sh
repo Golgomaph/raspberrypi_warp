26f34374777a1ab7a436783c16342b0347827a23
bawx25 <77675567+bawx25@users.noreply.github.com>
2021-02-01 14:42:24 +0100

    Update addwifi.sh

--- addwifi.sh
+++ addwifi.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-sed -i 4r<(sed '1,5!d' /etc/wpa_supplicant/wpa_template.template) /etc/wpa_supplicant/wpa_supplicant.conf"
+sed -i 4r<(sed '1,5!d' /etc/wpa_supplicant/wpa_template.template) /etc/wpa_supplicant/wpa_supplicant.conf
