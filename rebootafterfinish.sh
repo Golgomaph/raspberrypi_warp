459cfb770ec419fd331577cd4745d0c1b3dd775a
Golgomaph <74306376+Golgomaph@users.noreply.github.com>
2021-04-25 16:35:47 +0200

    Delete rebootafterfinish.sh

--- rebootafterfinish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-echo
-echo -n "----------------------------------------------------------------"
-echo
-echo -n "Installation finished. Reboot now? [y/N] "
-read REPLY
-if [[ ! "$REPLY" =~ ^(yes|y|Y)$ ]]; then exit 0; fi
-sudo reboot
